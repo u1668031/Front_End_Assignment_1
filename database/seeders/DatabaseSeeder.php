883b68caa17deeaefe930d19812bbeb875186fe0
Tom <tom_altham@hotmail.co.uk>
2021-11-07 16:07:17 +0000

    Add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,13 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Database\Seeders\WorkoutSeeder;
+use Database\Seeders\UserSeeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
         $this->call([
-            WorkoutSeeder::class
+            WorkoutSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
